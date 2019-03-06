49ba12dec533e637c6f920711b843d809e6b51a0
ocb-dev-04 <42700726+ocb-dev-04@users.noreply.github.com>
2019-03-06 16:49:58 -0400

    *Finish all page and codes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
+// code for show info
+
+function sendInfoContact(){
+
+    var name = document.getElementById('name_txt').value;
+    var last = document.getElementById('last_txt').value;
+    var email = document.getElementById('email_text').value;
+    var msg = document.getElementById('text_area').value;
+
+    alert("FullName:\n"+name + " " + last + "\n\nEmail: \n" + email + "\n\nMessage: \n" + msg);
+}
 
 
+// select subcription
+
+function showAlertWithSelect(typeSub){
+    showAlert("Select the " + typeSub + " Subscription");
+}
 
 function showAlert(message){
     alert(message);
